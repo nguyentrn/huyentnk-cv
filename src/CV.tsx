043bb63ebc3fb2e18bd638d8ad0ec54d07e3ad1d
Trần Nguyên <nguyentran0113@gmail.com>
2025-09-16 17:57:06 +0700

    update avatar

--- src/CV.tsx
+++ src/CV.tsx
@@ -1,3 +1,50 @@
+// function App() {
+//   return (
+//     <div
+//       className={
+//         "prose mx-auto flex h-[297mm] w-[210mm] max-w-none flex-col items-center px-20 py-24 shadow-lg shadow-red-500/50"
+//       }
+//     >
+//       <div id={"header"} className={"flex gap-16"}>
+//         <div className={"flex flex-col items-center"}>
+//           <h1
+//             className={
+//               "mb-0 flex self-start border-b-4 text-5xl leading-tight font-medium tracking-widest"
+//             }
+//           >
+//             TRƯƠNG NGUYỄN KHÁNH HUYỀN
+//           </h1>
+//           <table
+//             className={
+//               "max-w-sm [&>tbody>tr>td]:p-0.5 [&>tbody>tr>td]:text-base"
+//             }
+//           >
+//             <tbody>
+//               <tr className={"border-none"}>
+//                 <td className={"w-fit font-bold"}>Ngày sinh:</td>
+//                 <td>15/04/1999</td>
+//               </tr>
+//               <tr>
+//                 <td className={"w-fit font-bold"}>Email:</td>
+//                 <td>huyentnk1504@gmail.com</td>
+//               </tr>
+//             </tbody>
+//           </table>
+//         </div>
+//         <div className={"flex items-center gap-8"}>
+//           <div className="h-[180px] w-[120px] shrink-0 rounded-full bg-[url(avatar.png)] bg-cover"></div>
+//           <div className={"text-center text-2xl"}>
+//             Thực
+//             <br />
+//             tập
+//             <br /> sinh <br /> Marketing
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
 import { Header } from "@/features/resume/Header.tsx";
 import { Skills } from "@/features/resume/Skills.tsx";
 import { Educations } from "@/features/resume/Educations.tsx";
@@ -41,7 +88,6 @@ function App() {
         {/*<div className={"border-l-2"}>*/}
         <Projects lang={lang} />
         <Skills lang={lang} />
-
         {/*</div>*/}
         <Educations lang={lang} />
       </div>
--- src/features/resume/Avatar.tsx
+++ src/features/resume/Avatar.tsx
@@ -6,12 +6,7 @@ export const Avatar = () => {
       }
     >
       <div className={"relative h-40 w-40 overflow-hidden rounded-full"}>
-        <img
-          src={
-            "https://images.unsplash.com/photo-1494790108377-be9c29b29330?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          }
-          className={"relative -top-4 !my-0 bg-cover"}
-        />
+        <img src={"avatar.png"} className={"relative -top-4 !my-0 bg-cover"} />
       </div>
     </div>
   );
