20dc73bf2c7a4c2954df0b14b96b3020f09f34f5
Trần Nguyên <nguyentran0113@gmail.com>
2025-09-21 09:43:02 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import svgr from "vite-plugin-svgr";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svgr(), tailwindcss(), react()],
+  base: "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
