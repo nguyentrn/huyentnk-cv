666ae740fcb6bb20f87983fde2d7126c45474b3b
Trần Nguyên <nguyentran0113@gmail.com>
2025-10-02 23:49:40 +0700

    update

--- src/features/resume/Contact.tsx
+++ src/features/resume/Contact.tsx
@@ -1,15 +1,11 @@
 import { useResumeData } from "@/hooks/useResumeData";
-import { TbCake, TbMail, TbMapPinFilled, TbPhoneCall } from "react-icons/tb";
+import { TbMail, TbMapPinFilled, TbPhoneCall } from "react-icons/tb";
 
 export const Contact = () => {
   const { contact } = useResumeData();
 
   return (
     <div className={"-mb-3 flex justify-between border-y-1 py-3 text-sm"}>
-      <div className={"flex items-center gap-2"}>
-        <TbCake className={"relative text-lg text-rose-400"} />
-        {contact.birthYear}
-      </div>
       <div className={"flex items-center gap-2"}>
         <TbPhoneCall className={"relative text-lg text-rose-400"} />
         {contact.phone}
